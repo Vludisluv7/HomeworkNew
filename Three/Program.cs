d935ae43f7b4c971ff4572b332744bfc6f57d96a
Vludisluv <140197616+Vludisluv7@users.noreply.github.com>
2023-08-05 14:34:50 +0300

    Fin
    add

---
+++ Three/Program.cs
@@ -0,0 +1,45 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+
+
+int Message(string Message)
+{
+    System.Console.WriteLine(Message);
+    string value = Console.ReadLine();
+    int result = int.Parse(value);
+    return result;
+}
+
+bool WeekFunct(int DayWeek)
+{
+    if (DayWeek > 5)
+    {
+        return true;
+    }
+    return false
+}
+
+bool CheckWeekDay(int number)
+{
+    if (number > 0 || number <= 7)
+    {
+        return true;
+    }
+    System.Console.WriteLine("Таких дней недели не существует!");
+    return false;
+}
+
+int DayWeek = Message("Введите день недели -> ");
+if (CheckWeekDay(DayWeek))
+{
+    if (WeekFunct(DayWeek))
+    {
+        System.Console.WriteLine("Ура выходной)");
+    }
+    else
+    {
+        System.Console.WriteLine("Ну блин рабочий день(");
+    }
+}
---
+++ two/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+int Message(string Message)
+{
+    System.Console.WriteLine(Message);
+    string value = Console.ReadLine();
+    int result = int.Parse(value);
+    return result;
+}
+
+int GetThirdNum(int number)
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    return number % 10;
+}
+
+bool LowSizeNum(int number)
+{
+    if (number < 100)
+    {
+        System.Console.WriteLine("Третьей цифры нету");
+        return false;
+    }
+    return true;
+}
+
+int number = Message("Введите число -> ");
+if (LowSizeNum(number))
+{
+    System.Console.WriteLine(GetThirdNum(number));
+}
